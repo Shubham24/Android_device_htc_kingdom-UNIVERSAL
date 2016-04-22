1a94548d64cdc5760fdb8454c58db92c80176483
Shubham Sinha <sinhashubham24@gmail.com>
2016-04-22 17:57:41 -0400

    Update cm.mk

--- device/kingdom/cm.mk
+++ device/kingdom/cm.mk
@@ -3,3 +3,20 @@
 # Inherit device configuration
 $(call inherit-product, device/htc/kingdom/cm_kingdom.mk)
 
+# Inherit common CM configuration
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=htc_kingdom \
+    BUILD_FINGERPRINT="htc_wwe/htc_kingdom/vivo:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/test-keys" \
+    PRIVATE_BUILD_DESC="htc_kingdom-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) test-keys"
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_kingdom
+PRODUCT_GMS_CLIENTID_BASE := android-htc-rev
+
+
+
+# Release name
+PRODUCT_RELEASE_NAME := kingdom
